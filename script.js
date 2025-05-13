f2c4dba4b4f34d3612a1ec9f373f61d0047592b8
CashZwei <u.zimmermann@cash-online.de>
2025-05-13 16:57:48 +0200

    neue files zur aktivierung hochgeladen

--- script.js
+++ script.js
@@ -1,38 +1,198 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const eventElement = document.querySelector('.event');
-    const eventList = document.querySelector('.event-list');
+/* ARRAY UND HANDLING FÜR SPEAKER / LAYER SPEAKER */
+const speakers = [
+    {
+        name: 'Frank Rottenbacher',
+        position: 'Vorstand AfW Bundesverband Finanzdienstleistung e.V. / Going Public Akademie für Finanzberatung AG & Co. KG',
+        image: 'assets/rottenbacher.jpg',
+        infos: '',
+    },
+    {
+        name: 'Clemens Köbler',
+        position: 'Sales und Onboarding-Manager, Walnut',
+        image: 'assets/koebler.jpg',
+        infos: 'Clemens Köbler ist Sales und Onboarding Manager bei Walnut. Mit 20 Jahren Vertriebserfahrung und einem besonderen Gespür für die Praxis unterstützt er Finanzberater dabei, Walnut Live in ihren Arbeitsalltag zu integrieren - flexibel angepasst an ihre individuellen Bedürfnisse.',
 
-    window.addEventListener('scroll', () => {
-        if (window.innerWidth < 800) {
-            // Bei einer Breite unter 800px: Standardzustand (kein Scroll-Effekt)
-            eventElement.style.transform = `translateX(0%)`;
-            eventElement.style.opacity = 1;
-            return;
-        }
+    },
+    {
+        name: 'Frank Neumann',
+        position: 'Vertriebsdirektor, AUREUS Golddepot GmbH',
+        image: 'assets/neumann.jpg',
+        infos: 'Frank Neumann ist seit mehr als 40 Jahren in der Finanzbranche tätig. Er gründete 2010 mit der Exchange AG die Aureus Golddepot GmbH. Herr Neumann ist maßgeblich beteiligt an der Entwicklung der Produktlösungen der Aureus, sein Motto ist: Aus der Praxis für die Praxis!',
 
-        const scrollTop = window.scrollY; // Aktuelle Scrollposition
-        const windowHeight = window.innerHeight;
-        const eventListTop = eventList.offsetTop; // Position von event-list
+    },
+    {
+        name: 'Tanja Schmidt',
+        position: 'Vertriebsmanagerin, LV 1871',
+        image: 'assets/schmidt2.jpg',
+        infos: '',
 
-        // Bereich definieren: Animation beginnt bei 0%, endet bei 50% von 100vh
-        const startScroll = eventListTop - windowHeight; // Startpunkt (unterer Bildschirmrand)
-        const endScroll = eventListTop - windowHeight / 3; // Endpunkt (50% von 100vh)
+    }
+];
 
-        // Berechnung des Fortschritts (zwischen 0 und 1)
-        const scrollProgress = Math.min(
-            Math.max((scrollTop - startScroll) / (endScroll - startScroll), 0),
-            1
-        );
+const speakerArea = document.querySelector('.speaker-area');
 
-        // Transform und Opacity dynamisch setzen
-        eventElement.style.transform = `translateX(${(1 - scrollProgress) * 100}%)`;
-        eventElement.style.opacity = scrollProgress;
-    });
+for (let i = 0; i < speakers.length; i++) {
+    const speaker = speakers[i];
+    speakerArea.innerHTML += `
+        <div class="speaker-element" onclick="openLayer(${i})">
+            <img src="${speaker.image}" alt="${speaker.name}">
+            <div class="speaker-infos">
+                <p class="event-speaker-headline">${speaker.name}</p>
+                <div class="line"></div>
+                <p class="event-speaker-position grey-text">${speaker.position}</p>
+            </div>
+        </div>
+    `;
+}
+
+function openLayer(index) {
+    const speaker = speakers[index];
+    const layer = document.querySelector('.outter-layer-speaker');
+    layer.classList.remove('d-none');
+
+    document.body.style.overflow = 'hidden';
+
+    layer.innerHTML = `
+        <div class="inner-layer-speaker">
+            <div class="close" onclick="closeLayer()"><img src="assets/icons/close-white.svg" alt=""></div>
+            <div class="layer-top">
+                <img src="${speaker.image}" alt="${speaker.name}">
+                <div class="layer-speaker-infos">
+                    <p class="layer-speaker-headline">${speaker.name}</p>
+                    <p class="layer-speaker-position dark-text">${speaker.position}</p>
+                </div>
+            </div>
+            <div class="layer-speaker-text">${speaker.infos}</div>
+        </div>
+    `;
+}
+
+function closeLayer() {
+    document.querySelector('.outter-layer-speaker').classList.add('d-none');
+    document.body.style.overflow = 'auto';
+}
+
+/* CLOSE LAYER BEI CLICK AUSSERHALB INNER-LAYER */ 
+document.querySelector('.outter-layer-speaker').addEventListener('click', function (event) {
+    if (!event.target.closest('.inner-layer-speaker')) {
+        closeLayer();
+    }
 });
 
-// document.getElementById('redirect-button').onclick = () => {
-//     window.location.href = 'https://www.cash-online.de';
-// };
+
+
+
+
+
+
+/* ARRAY EVENTS */
+const events = [
+    {
+        time: '9.00 UHR',
+        title: 'Der EU AI Act – <br> Was kommt auf Vermittlerinnen und Vermittler zu?',
+        description: 'Die neue KI-Verordnung der EU verpflichtet Unternehmen mit KI-Systemen dazu, die erforderlichen KI-Kompetenzen ihrer Mitarbeitenden sicherzustellen. Doch was bedeutet das konkret für Vermittlerbetriebe?',
+        speakerName: 'Frank Rottenbacher',
+        speakerPosition: 'Vorstand AfW Bundesverband Finanzdienstleistung e.V. / Going Public Akademie für Finanzberatung AG & Co. KG',
+        speakerImage: 'assets/rottenbacher.jpg',
+        link: 'https://cash.clickmeeting.com/der-eu-ai-act-was-kommt-auf-vermittlerinnen-und-vermittler-zu-/register',
+        idd: '15'
+    },
+    {
+        time: '10.15 UHR',
+        title: 'Vom Erstkontakt zur digitalen Zeichnung: <br>So optimieren Sie Ihre Beratung mit Walnut Live',
+        description: 'Ein Haken falsch gesetzt – und der Antrag geht zurück. Ein Tool zu viel – und der Kunde springt ab. Kommt Ihnen das bekannt vor? In diesem Webinar erfahren Sie, wie Sie mit Walnut Live den gesamten Beratungs- und Zeichnungsprozess für Privatmarktfonds digital und durchgängig abbilden: von der fehlerfreien Antragserstellung über die digitale Legitimation bis hin zur elektronischen Zeichnung – FinVermV-konform, browserbasiert und ohne Medienbrüche.',
+        speakerName: 'Clemens Köbler',
+        speakerPosition: 'Sales und Onboarding-Manager, Walnut',
+        speakerImage: 'assets/koebler.jpg',
+        link: 'https://cash.clickmeeting.com/vom-erstkontakt-zur-digitalen-zeichnung-so-optimieren-sie-ihre-beratung-mit-walnut-live/register'
+    },
+    {
+        time: '11.30 UHR',
+        title: 'Die Zukunftschance: <br>Silber - Platin - Palladium',
+        description: 'Gold boomt aktuell: Aber warum haben die Weißmetalle ein enormes Wachstumspotenzial? Die Edelmetallquote der Deutschen liegt bei unter drei Prozent und bietet ein enormes Umsatzpotenzial. Lernen Sie die USPs der Aureus kennen – mit einfacher digitaler Abwicklung.',
+        speakerName: 'Frank Neumann',
+        speakerPosition: 'Vertriebsdirektor, AUREUS Golddepot GmbH',
+        speakerImage: 'assets/neumann.jpg',
+        link: 'https://cash.clickmeeting.com/die-zukunftschance-silber-platin-palladium/register',
+        idd: '25'
+    },
+    {
+        time: '14.00 UHR',
+        title: 'Effizienter Beraten - <br>mit den digitalen Tools der LV 1871',
+        description: 'Die private Altersvorsorge ist ein komplexes und beratungsintensives Thema, an dem heute kein Weg mehr vorbeiführt. Doch: Kaum jemand möchte mehr als zwei Stunden Zeit investieren, um mit einer Beraterin oder einem Berater über die Absicherung in der Rente zu sprechen. Entsprechend wichtig ist der Einsatz von digitalen Tools im Maklergeschäft. Die Angebotssoftware der LV 1871 ermöglicht unabhängigen Maklerinnen und Maklern einen schnellen und reibungslosen Prozess im Beratungsgespräch. Um die Beratung so effizient wie möglich zu gestalten, wird Tanja Schmidt Vertriebsmanagerin der LV 1871 Ihnen alles Nötige in diesem Webinar mit an die Hand geben.',
+        speakerName: 'Tanja Schmidt',
+        speakerPosition: 'Vertriebsmanagerin LV 1871',
+        speakerImage: 'assets/schmidt2.jpg',
+        link: 'https://cash.clickmeeting.com/effizienter-beraten-mit-den-digitalen-tools-der-lv-1871/register',
+        idd: '35'
+    }
+];
+
+const eventContent = document.querySelector('.event-content');
+
+for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    eventContent.innerHTML += `
+        <div class="event">
+            <div class="event-time">${event.time}</div>
+            <div class="event-infos">
+                <div class="event-title">
+                    ${event.title}
+                    ${event.idd ? `<div class="idd">${event.idd} IDD-Minuten</div>` : ''}
+                </div>
+                ${event.idd ? `<div class="idd-mobile">${event.idd} IDD-Minuten</div>` : ''}
+                <div class="separator" style="height: 2px"></div>
+                <div class="event-description">
+                    ${event.description}
+                </div>
+                <div class="separator" style="height: 2px"></div>
+                <div class="event-speaker">
+                    <div class="event-speaker-infos">
+                        <p class="event-speaker-headline dark-text" style="padding-bottom: 10px;">SPEAKER:</p>
+                        <p class="event-speaker-headline">${event.speakerName}</p>
+                        <p class="event-speaker-position dark-text">${event.speakerPosition}</p>
+                    </div>
+                    <a href="${event.link}" target="_blank">
+                        <button class="signup">           
+                            JETZT ANMELDEN
+                            <img src="assets/icons/forward-white.svg" alt="">
+                        </button>
+                    </a>
+                    <img class="portrait" src="${event.speakerImage}" alt="${event.speakerName}">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+
+
+
+
+
+
+/* HEADER NUR SICHTBAR BEI SCROLL UP */ 
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        header.style.top = '-100px';
+    } else {
+        header.style.top = '0';
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Für Safari
+});
+
+
+
+
+
+
 
 function redirectToCash() {
     window.location.href = 'https://www.cash-online.de';
